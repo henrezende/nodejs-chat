4d198e7e048d2fec9021ed9a7e75071e7569f027
Henrique Rezende <henrique.64@hotmail.com>
2024-03-16 18:56:31 -0300

    feat: validate repeated users

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -12,21 +12,52 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
   private connectedUsers: Map<string, string> = new Map();
+  private activeUsernames: Map<string, number> = new Map();
 
   handleConnection(client: Socket) {
     const username = client.handshake.query.username as string;
-    this.connectedUsers.set(client.id, username);
+    const validatedUsername = this.validateUsername(username);
+    this.connectedUsers.set(client.id, validatedUsername);
     this.notifyUserListUpdate();
+    this.server.emit('validatedUsername', {
+      name: validatedUsername,
+      id: client.id,
+    });
   }
 
   handleDisconnect(client: Socket) {
+    const username = client.handshake.query.username as string;
+
     this.connectedUsers.delete(client.id);
     this.notifyUserListUpdate();
+    this.updateActiveUsernames(username);
   }
 
   notifyUserListUpdate() {
     const userList = Array.from(this.connectedUsers.values());
     this.server.emit('userListUpdate', userList);
+  }
+
+  validateUsername(username: string): string {
+    let count = this.activeUsernames.get(username) || 0;
+    if (count > 0) {
+      count++;
+      this.activeUsernames.set(username, count);
+      return `${username} (${count})`;
+    } else {
+      this.activeUsernames.set(username, 1);
+      return username;
+    }
+  }
+
+  updateActiveUsernames(username: string): void {
+    let countOfCurrentUsername = this.activeUsernames.get(username);
+    if (countOfCurrentUsername > 1) {
+      countOfCurrentUsername--;
+      this.activeUsernames.set(username, countOfCurrentUsername);
+    } else {
+      this.activeUsernames.delete(username);
+    }
   }
 
   @SubscribeMessage('message')
