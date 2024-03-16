ba37b2dd0aec88bc5700d5d489b4e684db96979b
Henrique Rezende <henrique.64@hotmail.com>
2024-03-16 13:25:25 -0300

    feat: list of connected users server-side

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -11,12 +11,22 @@ import { Server, Socket } from 'socket.io';
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
+  private connectedUsers: Map<string, string> = new Map();
+
   handleConnection(client: Socket) {
-    console.log(`Usuário conectado: ${client.id}`);
+    const username = client.handshake.query.username as string;
+    this.connectedUsers.set(client.id, username);
+    this.notifyUserListUpdate();
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Usuário desconectado: ${client.id}`);
+    this.connectedUsers.delete(client.id);
+    this.notifyUserListUpdate();
+  }
+
+  notifyUserListUpdate() {
+    const userList = Array.from(this.connectedUsers.values());
+    this.server.emit('userListUpdate', userList);
   }
 
   @SubscribeMessage('message')
